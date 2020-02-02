b6b39af22cd0be235912addeec8571bd30b137ad
sh-gif <kharelshova@gmail.com>
2020-02-02 11:21:46 -0600

    my first java commit

---
+++ src/MyFirstJavaProgram.java
@@ -0,0 +1,31 @@
+// Comments in java stats with //. Compiler ignores that line.
+
+// Compiler : Compiler converts Source code ( the program that we write such as MyFirstJavaProgram) to
+// Machine Code ( 0/1 )
+
+// Every computer has a grammar that is called syntax. you have to follow syntax to get the correct output.
+
+// Every java program has a class Syntax: public class <ClassName> { ............ } Note: ClassName should match file name.
+
+// function: is something that accept inputs, processes it and generates output. e.g f(x) = x^2.
+// f(2) = 4 (input = 2, output = 4)
+// Note all functions might not generate output.
+
+// class definition: is a collection of functions.
+
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+        // to print java provides function called System.out.println(....)
+        //short cut for System.out.println is sout.
+        // Character definition: keyboards keys, ka kha...
+        // String definition: collection of characters shova, subodh, supreme. Strings are kept inside double quotes eg "Shova is learning java"
+        // Each instruction in java programs ends with semi colon.
+
+        System.out.println("My first java program"); // see the result in console or terminal
+        System.out.println("Hurrahay I have completed my first java program");
+    }
+
+}
+
+// Create a new class with name FirstJavaProgram that prints "I am doing well" on a console.
