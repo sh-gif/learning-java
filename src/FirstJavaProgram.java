79acba334bf9dd7e24eafe3ccf5f104a0ebf98ad
sh-gif <kharelshova@gmail.com>
2020-02-05 22:32:59 -0600

    renamed

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("I am doing well");
+        System.out.println("I am learning how to use git.");
+    }
+
+}
+
+
