cf44f0983a9fc53dbb4559b789646581daf556ed
sh-gif <kharelshova@gmail.com>
2020-02-05 21:23:00 -0600

    added new line

--- src/homework1.java
+++ src/homework1.java
@@ -1,5 +1,6 @@
 public class homework1 {
     public static void main(String[] args) {
        System.out.println("I am doing well");
+       System.out.println("I am learning how to use git.");
     }
 }
