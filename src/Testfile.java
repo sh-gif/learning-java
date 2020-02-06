c0db51278a412eaec96839135944116409747a5b
sh-gif <kharelshova@gmail.com>
2020-02-05 21:55:08 -0600

    third commit

---
+++ src/Testfile.java
@@ -0,0 +1,9 @@
+public class Testfile
+{
+    public static void main(String[] args) {
+
+        System.out.println("learning java is kind of fun");
+
+
+    }
+}
--- src/homework1.java
+++ src/homework1.java
@@ -3,4 +3,10 @@ public class homework1 {
        System.out.println("I am doing well");
        System.out.println("I am learning how to use git.");
     }
-}
+
+    public static void main(String[] args) {
+        System .out.println("learning java programming is kind of fun.");
+
+    }
+
+
